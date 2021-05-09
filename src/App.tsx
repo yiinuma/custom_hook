15de17d9602a3d9ae82f27216ee95d5eb1b861b8
yiinuma <65111001+yiinuma@users.noreply.github.com>
2021-05-09 21:26:01 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { useState } from "react";
+import { UserCard } from "./componets/UserCard";
+import "./styles.css";
+import { User } from "./types/api/user";
+import { UserProfile } from "./types/UserProfile";
+
+export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onClickFetUser = () => {
+    setLoading(true);
+    setError(false);
+
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+  return (
+    <div className="App">
+      <button onClick={onClickFetUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+}
